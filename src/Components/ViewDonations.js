165575c158555345744ce1860039047324ca6a53
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-02 20:53:19 +0545

    view donations state

--- src/Components/ViewDonations.js
+++ src/Components/ViewDonations.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import Axios from 'axios'
 import NavBar from './NavBar'
+
+
 export default class ViewDonations extends Component {
+    constructor(props) {
+        super(props)
+
+    this.state = {
+        foodname: '',
+        foodtype: '',
+        country: '',
+        district: '',
+        street: '',
+        phone: '',
+        date: '',
+        config: {
+            headers: { 'Authorization': localStorage.getItem('token') }
+        }
+    }
+}
+
   render() {
   return (
     <div>
       <NavBar history = {this.props.history}/>
-     
+      
       </div>
   )
 }
