d035ae44361fe96f61221f173691e28ca27a050d
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-02 10:53:07 +0545

    handleChange

--- src/Components/AddRequest.js
+++ src/Components/AddRequest.js
@@ -22,7 +22,13 @@ export default class AddRequest extends Component{
             }
         }
     }
-   
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        }, () => console.log(this.state))
+    }
+    
     render(){
         return(
             <div>
