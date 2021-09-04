f519cab87c83eb0d196246b209874ff3042a60d9
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-04 21:07:33 +0545

    profile created

---
+++ src/Components/Profile.js
@@ -0,0 +1,20 @@
+import React  from 'react'
+import axios from 'axios'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import jwt_decode from 'jwt-decode'
+import NavBar from './NavBar'
+
+export default class Profile extends React.Component{
+    
+    render() {
+        return(
+            <div>
+                <NavBar history = {this.props.history}/>
+         
+          
+</div>
+
+    )
+}
+
+}
