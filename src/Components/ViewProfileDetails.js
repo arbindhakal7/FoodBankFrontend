aa052923c0e264293cbddf6b2c7a62e3d5dc9a6f
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-04 21:19:23 +0545

    form created

--- src/Components/ViewProfileDetails.js
+++ src/Components/ViewProfileDetails.js
@@ -27,7 +27,21 @@ export default class ViewProfileDetails extends React.Component{
             <div>
                 <NavBar history = {this.props.history}/>
             
-                              </div>
+                <div id="main" class="wrapper style1">
+                <section class="container">
+                        <section class="4u">   
+                            <h3>Your Info</h3>
+                            <p> Full Name : <i>{this.state.firstName} </i></p>
+                            <p>Phone: <i>{this.state.phone}</i></p>
+                            <p>Role: <i>{this.state.role}</i></p>
+                            <p>Email: <i>{this.state.email}</i></p>
+                            <p>Date Of Birth: <i>{this.state.dateOfBirth}</i></p>
+                            <p>Gender: <i>{this.state.gender}</i></p>
+                        </section>
+                    
+                </section> 
+                </div>
+                </div>
            
         )
     }
