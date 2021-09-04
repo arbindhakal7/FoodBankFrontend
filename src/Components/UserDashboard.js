1e5ca0383e719814ab173efe254f256c726050ed
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-04 21:48:48 +0545

    updated

--- src/Components/UserDashboard.js
+++ src/Components/UserDashboard.js
@@ -1,92 +1,94 @@
 
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import NavBar from './NavBar'
 import jwt_decode from 'jwt-decode'
 import foodbanks from '../css/images/FoodBanks.jpg'
 import { Link } from 'react-router-dom';
 import users from '../css/images/users.png';
 
-export default class UserDashboard extends Component{
+export default class UserDashboard extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            profileId:'',
-            username: '',
-            phone:'',
-            role:'',
+            profileId: '',
+            fullname: '',
+            phone: '',
+            role: '',
             email: '',
             dateOfBirth: '',
             gender: '',
-            lastDonation:'',
-            lastRequest:'',
+            lastDonation: '',
+            lastRequest: '',
             foodbanks: [],
-            foodbankId:'',
+            foodbankId: '',
             FoodBankName: '',
-            availableFood:'',    
+            availableFood: '',
             config: {
                 headers: { 'Authorization': localStorage.getItem('token') }
             }
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const token = localStorage.getItem('token')
-        const decoded=jwt_decode(token)
-          this.setState({
-            username: decoded.username,
+        const decoded = jwt_decode(token)
+        this.setState({
+            fullname: decoded.fullname,
             phone: decoded.phone,
             role: decoded.role,
-            email:decoded.email ,
+            email: decoded.email,
             dateOfBirth: decoded.dateOfBirth,
             gender: decoded.gender,
-            lastDonation:decoded.lastDonation,
-            lastRequest:decoded.lastRequest,
-            image:decoded.image,
+            lastDonation: decoded.lastDonation,
+            lastRequest: decoded.lastRequest,
+            image: decoded.image,
             UserId: decoded.id
-    
-            
-          })
-        }
-        
-    
+
+
+        })
+    }
+
+
     render() {
-        return(
+        return (
             <div>
-                <NavBar history = {this.props.history}/>
+                <NavBar history={this.props.history} />
                 <div id="main" class="wrapper style1">
-                <section class="container">
-                    <header class="major">
-                        <h2>Hello, {this.state.username}</h2>
-                        <span class="byline">This is User Dashboard</span>
-                    </header>
-                    <div class="row no-collapse-1">
-                        <section class="4u">   
-                            <a href="!#" class="image feature"></a>
-                            <img src={users} alt="Users" />
-                            <h3>Your Info</h3>
-                            <p>Full Name : <i>{this.state.username}</i></p>
-                            <p>Phone Number : <i>{this.state.phone}</i></p>
-                            <p>Email : <i>{this.state.email}</i></p>
+                    <section class="container">
+                        <header class="major">
+                            <h2>Hello, {this.state.fullname}</h2>
+                            <br/>
+                            <span class="byline">Welcome to the User Dashboard</span>
+                        </header>
+                        <div class="row no-collapse-1">
+                            <section class="4u">
+                                <a href="!#" class="image feature"></a>
+                                <img src={users} alt="Users" />
+                                <h3>Your Info</h3>
+                                <p>Full Name : <i>{this.state.fullname}</i></p>
+                                <p>Phone Number : <i>{this.state.phone}</i></p>
+                                <p>Email : <i>{this.state.email}</i></p>
 
-                            <Link class="btn btn-primary mr-2" to='/userdashboard/profile/:id'> Edit </Link>
-                        </section>
-                        <section class="4u">                            
-                            <a href="/!#" class="image feature"></a>
-                            <img src={foodbanks} alt="foodbank" />
-                            <h3>Food Bank</h3>
-                            {/* <FoodBankList/> */}
+                                <Link class="btn btn-primary mr-2" to='/userdashboard/profile/:id'> Edit </Link>
+                            </section>
+                            <br/>
+                            <section class="4u">
+                                <a href="/!#" class="image feature"></a>
+                                <img src={foodbanks} alt="foodbank" />
+                                <h3>Food Bank</h3>
+                                {/* <FoodBankList/> */}
 
-    
-                        </section>
-                    </div>
-                </section> 
-                </div> 
+
+                            </section>
+                        </div>
+                    </section>
+                </div>
             </div>
         )
     }
 
-    
+
 }
 
 
