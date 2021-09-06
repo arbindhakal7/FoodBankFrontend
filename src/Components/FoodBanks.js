8a76e190c0e5fecc1c2f4d880a0e602c26d928d4
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-06 22:23:13 +0545

    constructor

--- src/Components/FoodBanks.js
+++ src/Components/FoodBanks.js
@@ -5,7 +5,20 @@ import NavBar from './NavBar'
 
 export default class FoodBank extends Component {
 
-  
+    constructor(props){
+
+        super(props)
+    
+        this.state = {
+          foodbanks: [],
+          foodbankId:'',
+          FoodBankName: '',
+          availableFood:'',
+            config: {
+                headers: { 'Authorization': localStorage.getItem('token') }
+            }
+        }
+      }
 
 render() {
   return (
