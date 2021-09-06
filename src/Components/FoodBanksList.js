a55747c7a08052cbb8defc63f7f5664f647cc63c
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-06 22:28:41 +0545

    FoodBankList initiated

---
+++ src/Components/FoodBanksList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import Axios from 'axios'
+
+export default class BloodBanks extends Component {
+
+
+render() {
+  return (
+      <div className='container'>
+  
+      </div>
+  )
+}
+}
+
+
+
+
+
+
