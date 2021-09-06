28a2801b062210008c37b268d7af53503e0c8549
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-06 22:29:15 +0545

    constructor

--- src/Components/FoodBanksList.js
+++ src/Components/FoodBanksList.js
@@ -3,7 +3,21 @@ import { Link } from 'react-router-dom';
 import Axios from 'axios'
 
 export default class BloodBanks extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          foodbanks: [],
+          foodbankId: "",
+          FoodBankName: "",
+          availableFood: "",
+          config: {
+            headers: { Authorization: localStorage.getItem("token") },
+          },
+        };
+      }
 
+      
 
 render() {
   return (
