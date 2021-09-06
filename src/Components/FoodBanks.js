520b8bc9997eec1c9e143d1ddbca4768c35e46af
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-06 22:25:34 +0545

    completed

--- src/Components/FoodBanks.js
+++ src/Components/FoodBanks.js
@@ -1,48 +1,61 @@
-import React, { Component } from 'react'
-import Axios from 'axios'
-import NavBar from './NavBar'
-
+import React, { Component } from "react";
+import Axios from "axios";
+import NavBar from "./NavBar";
 
 export default class FoodBank extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
+    this.state = {
+      foodbanks: [],
+      foodbankId: "",
+      FoodBankName: "",
+      availableFood: "",
+      config: {
+        headers: { Authorization: localStorage.getItem("token") },
+      },
+    };
+  }
 
-        super(props)
-    
-        this.state = {
-          foodbanks: [],
-          foodbankId:'',
-          FoodBankName: '',
-          availableFood:'',
-            config: {
-                headers: { 'Authorization': localStorage.getItem('token') }
-            }
-        }
-      }
+  componentDidMount() {
+    Axios.get("http://localhost:90/api/FoodBank", this.state.config)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          foodbanks: res.data,
+        });
+      })
+      .catch((err) => console.log(err.response));
+  }
 
-      componentDidMount(){
-        Axios.get('http://localhost:90/api/FoodBank', this.state.config)
-        .then((res)=> {
-          console.log(res.data)
-          this.setState({
-              foodbanks: res.data
-          })
-        }).catch(err => console.log(err.response));
-      }
-
-      
-
-render() {
-  return (
-    <div>
-      <NavBar history = {this.props.history}/>
-     </div>
-  )
+  render() {
+    return (
+      <div>
+        <NavBar history={this.props.history} />
+        <div className="container">
+          <div className="py-4">
+            <h1>Food Bank List</h1>
+            <table class="table border shadow">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Food Bank Name</th>
+                  <th scope="col">Available Food Type</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.foodbanks.map((foodbank) => (
+                  <tr key={foodbank._id}>
+                    <th scope="row"></th>
+                    <td>{foodbank.FoodBankName}</td>
+                    <td>{foodbank.availableFood}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-}
-
-
-
-
-
-
