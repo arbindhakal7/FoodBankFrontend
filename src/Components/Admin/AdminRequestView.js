c49efbe2664ecce93b7a9c1e2d5154b2e12fa3b0
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 22:10:38 +0545

    handles and componentDidMount

--- src/Components/Admin/AdminRequestView.js
+++ src/Components/Admin/AdminRequestView.js
@@ -2,6 +2,56 @@ import React, { Component } from "react";
 import AdminNavBar from "./AdminNavBar";
 
 export default class AdminViewRequests extends Component {
+    constructor(props){
+    super(props)
+        this.state = {
+          requestfoods: [],
+          requestId:'',
+            requestName: '',
+            phone: '',
+            foodtype:'',
+            country: '',
+            district: '',
+            street: '',
+            date:'',
+            config: {
+                headers: { 'Authorization': localStorage.getItem('token') }
+            }
+        }
+      }
+    
+      handleDelete = (id) => {
+        if(window.confirm('Are you sure to remove this request from the list?'))
+        Axios.delete('http://localhost:90/api/RequestFood/' + id, this.state.config)
+        .then((res)=> {
+            const filteredReqFood = this.state.requestfoods.filter(req => {
+                return req._id !== id;
+            });
+            console.log(filteredReqFood);
+            this.setState({
+                requestbloods: filteredReqFood
+            });
+         
+        }).catch(err => console.log(err.response));
+      }
+    
+      handleViewClick = (id) => {
+          console.log(id)
+          this.props.history.push(`/admindashboard/adminviewrequestdetails/${id}`);
+      }
+    
+    
+    componentDidMount(){
+      Axios.get('http://localhost:90/api/admin/requests', this.state.config)
+      .then((res)=> {
+        console.log(res.data)
+        this.setState({
+          requestbloods: res.data
+        })
+      }).catch(err => console.log(err.response));
+    }
+
+
   render() {
     return 
     <div>
