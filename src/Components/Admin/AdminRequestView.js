11ce914b87e1de46ee06c8bb53a1607230ef0b55
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 22:06:18 +0545

    Request View for Admin

---
+++ src/Components/Admin/AdminRequestView.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import AdminNavBar from "./AdminNavBar";
+
+export default class AdminViewRequests extends Component {
+  render() {
+    return 
+    <div>
+     <AdminNavBar history = {this.props.history}/>
+
+    </div>;
+  }
+}
