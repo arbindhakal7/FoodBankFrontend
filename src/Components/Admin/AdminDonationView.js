aa48a45ef3d6f27c2e93736a3e68b9836a752ced
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 21:29:27 +0545

    Donation View done

--- src/Components/Admin/AdminDonationView.js
+++ src/Components/Admin/AdminDonationView.js
@@ -21,40 +21,89 @@ export default class AdminViewDonations extends Component {
   }
 
   handleDelete = (id) => {
-    if(window.confirm('Are you sure to remove this donation from the list?'))
-	Axios.delete('http://localhost:90/api/DonateFood/' + id, this.state.config)
-	.then((res)=> {
-		const filteredDonateFood = this.state.donatefoods.filter(req => {
-			return req._id !== id;
-		});
-		console.log(filteredDonateFood);
-		this.setState({
-			donatefoods: filteredDonateFood
-		});
-	 
-	}).catch(err => console.log(err.response));
-  }
+    if (window.confirm("Are you sure to remove this donation from the list?"))
+      Axios.delete(
+        "http://localhost:90/api/DonateFood/" + id,
+        this.state.config
+      )
+        .then((res) => {
+          const filteredDonateFood = this.state.donatefoods.filter((req) => {
+            return req._id !== id;
+          });
+          console.log(filteredDonateFood);
+          this.setState({
+            donatefoods: filteredDonateFood,
+          });
+        })
+        .catch((err) => console.log(err.response));
+  };
 
   handleUpdateClick = (id) => {
-	  console.log(id)
-	  this.props.history.push(`/admindashboard/updatedonationstatus/${id}`);
+    console.log(id);
+    this.props.history.push(`/admindashboard/updatedonationstatus/${id}`);
+  };
+
+  componentDidMount() {
+    Axios.get("http://localhost:90/api/admin/donations", this.state.config)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          donatefoods: res.data,
+        });
+      })
+      .catch((err) => console.log(err.response));
   }
-
-componentDidMount(){
-  Axios.get('http://localhost:90/api/admin/donations', this.state.config)
-  .then((res)=> {
-    console.log(res.data)
-    this.setState({
-        donatefoods: res.data
-    })
-  }).catch(err => console.log(err.response));
-}
-
 
   render() {
     return (
       <div>
         <AdminNavBar history={this.props.history} />
+        <div className="container">
+          <div className="py-4">
+            <h1>Donation List</h1>
+            <table class="table border shadow">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Full Name</th>
+                  <th scope="col">Full Address</th>
+                  <th scope="col"> Phone</th>
+                  <th scope="col"> Food Type</th>
+
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.donatefoods.map((donation) => (
+                  <tr key={donation._id}>
+                    <th scope="row"></th>
+                    <td>{donation.donorName}</td>
+                    <td>
+                      {donation.country},{donation.district},
+                      {donation.street}
+                    </td>
+                    <td>{donation.phone}</td>
+                    <td>{donation.foodtype}</td>
+                    <td>
+                      <Link
+                        class="btn btn-outline-primary mr-2"
+                        onClick={() => this.handleUpdateClick(donation._id)}
+                      >
+                        Edit
+                      </Link>
+                      <Link
+                        class="btn btn-danger"
+                        onClick={() => this.handleDelete(donation._id)}
+                      >
+                        Delete
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
