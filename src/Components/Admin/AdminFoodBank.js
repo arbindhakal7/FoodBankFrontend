f45d14ca2b17ef92d11a1c9687d8e9e8c5e8f24f
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 08:50:56 +0545

    constructor, functions and component added

--- src/Components/Admin/AdminFoodBank.js
+++ src/Components/Admin/AdminFoodBank.js
@@ -1,14 +1,56 @@
 import React, { Component } from "react";
-import AdminNavBar from './AdminNavBar';
-
-
+import AdminNavBar from "./AdminNavBar";
 
 export default class AdminFoodBank extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foodbanks: [],
+      foodbankId: "",
+      FoodBankName: "",
+      availableFood: "",
+      config: {
+        headers: { Authorization: localStorage.getItem("token") },
+      },
+    };
+  }
+
+  handleDelete = (id) => {
+    if (window.confirm("Are you sure to remove this Food bank from the list?"))
+      Axios.delete("http://localhost:90/api/FoodBank/" + id, this.state.config)
+        .then((res) => {
+          const filteredfoodBank = this.state.foodbanks.filter((foodBank) => {
+            return foodBank._id !== id;
+          });
+          console.log(filteredfoodBank);
+          this.setState({
+            foodbanks: filteredfoodBank,
+          });
+        })
+        .catch((err) => console.log(err.response));
+  };
+
+  handleUpdateClick = (id) => {
+    console.log(id);
+    this.props.history.push(`/admindashboard/updatefoodbank/${id}`);
+  };
+
+  componentDidMount() {
+    Axios.get("http://localhost:90/api/FoodBank", this.state.config)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          foodbanks: res.data,
+        });
+      })
+      .catch((err) => console.log(err.response));
+  }
+
   render() {
     return (
       <div>
         <AdminNavBar history={this.props.history} />
-        
       </div>
     );
   }
