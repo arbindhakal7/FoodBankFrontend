22d0abf76acc57b004915ec81199c96769c0d85a
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 08:54:15 +0545

    constructor and componentDidMount added

--- src/Components/Admin/AdminUserList.js
+++ src/Components/Admin/AdminUserList.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Axios from 'axios'
+
 
 export default class AdminUserList extends Component {
-    render() {
-        return (
-          <div> 
-              
-          </div>
-          )
-        }
-        }
-        
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      userId: "",
+      fullname: "",
+      phone: "",
+      address: "",
+      role: "",
+      config: {
+        headers: { Authorization: localStorage.getItem("token") },
+      },
+    };
+  }
+
+  componentDidMount() {
+    Axios.get("http://localhost:90/api/admin/users", this.state.config)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          users: res.data,
+        });
+      })
+      .catch((err) => console.log(err.response));
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
