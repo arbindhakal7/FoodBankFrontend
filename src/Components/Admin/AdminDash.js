a05ed427df6516de1e656a3a31e5771c0dfee4ed
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 08:22:37 +0545

    constructor and componentDidMount added

--- src/Components/Admin/AdminDash.js
+++ src/Components/Admin/AdminDash.js
@@ -1,6 +1,37 @@
 import React, {Component} from 'react'
 
 export default class AdminDashboard extends Component{
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            fullname: '',
+            phone:'',
+            role:'',
+            email: '',
+            dateOfBirth: '',
+            gender: '',
+            config: {
+                headers: { 'Authorization': localStorage.getItem('token') }
+            }
+        }
+    }
+    componentDidMount(){
+    const token = localStorage.getItem('token')
+    const decoded=jwt_decode(token)
+      this.setState({
+        fullname: decoded.fullname,
+        phone: decoded.phone,
+        role: decoded.role,
+        email:decoded.email ,
+        dateOfBirth: decoded.dateOfBirth,
+        gender: decoded.gender,
+        image:decoded.image,
+        UserId: decoded.id,
+      })
+    }
+    
     render() {
         return(
             <div>
