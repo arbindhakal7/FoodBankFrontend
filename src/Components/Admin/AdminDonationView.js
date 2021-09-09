f87855e612b4932ba9ecd1f787db5e061b622980
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 21:07:03 +0545

    View donations for admin

---
+++ src/Components/Admin/AdminDonationView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import AdminNavBar from './AdminNavBar'
+
+export default class AdminViewDonations extends Component {
+
+render() {
+  return (
+    <div>
+    <AdminNavBar history = {this.props.history}/>
+    
+    </div>
+  )
+}
+}
+
+
+
+
+
+
