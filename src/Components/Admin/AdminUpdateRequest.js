136841436c749ea4a206cf1ba2a82ebcea92c2f3
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 22:42:06 +0545

    constructor and function

--- src/Components/Admin/AdminUpdateRequest.js
+++ src/Components/Admin/AdminUpdateRequest.js
@@ -14,6 +14,62 @@ export default function UpdatedRequest(props) {
 }
 
  class UpdateForm extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+			id: this.props.id,
+            requestName: '',
+            phone: '',
+            foodtype:'',
+            country: '',
+            district: '',
+            street: '',
+			date:'',
+            config: {
+                headers: { 'Authorization': localStorage.getItem('token') }
+            }
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        if(window.confirm('Are you sure to update your request details?'))
+        event.preventDefault();
+		axios.put('http://localhost:90/api/RequestFood/' + this.props.id, this.state, this.state.config)
+		.then((res) => {
+			console.log(res)
+		}).catch(err => console.log(err.response.data));
+            
+    }
+
+
+	handleUpdate = () => {
+		
+	}
+	
+	componentDidMount = () => {
+		axios.get('http://localhost:90/api/RequestFood/' + this.state.id, this.state.config)
+		.then((res) => {
+			console.log(res);
+			this.setState({
+				requestName: res.data.requestName,
+				phone: res.data.phone,
+				foodtype: res.data.foodtype,
+				country: res.data.country,
+				district: res.data.district,
+				street: res.data.street,
+				date:res.data.date
+				
+			})
+		}).catch(err => console.log(err.response.data));
+	}
+
+
 
     render(){
         return(
