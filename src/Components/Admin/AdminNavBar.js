591740ffc1e7f6ec2b066b1fbece6e3257f53ba0
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 08:34:49 +0545

    logout handle

--- src/Components/Admin/AdminNavBar.js
+++ src/Components/Admin/AdminNavBar.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 export default function AdminNavBar(props) {
 
+    const handleLogout = () => {
+        if(window.confirm('Do you want to logout?'))
+        localStorage.removeItem('token');
+        props.history.push('/');
+        
+    }
+    
 return(
 <div>
-    
+
 </div>
 )
 }
