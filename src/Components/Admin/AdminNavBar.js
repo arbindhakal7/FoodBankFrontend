d60ac77c7d32fe036acdbe476afd17c60535d731
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 08:35:48 +0545

    admin nave done

--- src/Components/Admin/AdminNavBar.js
+++ src/Components/Admin/AdminNavBar.js
@@ -8,9 +8,49 @@ export default function AdminNavBar(props) {
         props.history.push('/');
         
     }
-    
+
 return(
-<div>
+<div> 
+    <Navbar color='dark' dark expand='md'>
+            <Nav id="nav" className='mr-auto' >
+                    <ul id="navul">
+                    <NavItem id="navitem">
+                            <Link to='/admindash/nav'>Dashboard</Link>                            
+                        </NavItem>
+                       <NavItem id="navitem">
+                            <Link to='/admindash/nav'>Admin Profile</Link>
+                            <ul>
+                                <Link to='/admindashboard/viewprofile' id="navlist">View Profile</Link>
+                                <Link to='/admindashboard/updateprofile' id="navlist">Edit Profile</Link>
+                            </ul>
+                        </NavItem>
+                        <NavItem id="navitem">
+                            <NavLink to='/admindashboard/adminviewusers'>Users List</NavLink>
+                        </NavItem >
+                        <NavItem id="navitem">
+                            <NavLink to='/admindashboard/adminviewdonations'>Donations List</NavLink>
+                        </NavItem >
+                        <NavItem id="navitem">
+                            <NavLink to='/admindashboard/adminviewrequests'>Requests List</NavLink>
+                        </NavItem>
+                        <NavItem id="navitem">
+                            <NavLink to='/admindash/nav'>Food Bank</NavLink>
+                            <ul>
+                                <Link to='/admindashboard/adminaddfoodbank' id="navlist">Add Food Bank</Link>
+                                <Link to='/admindashboard/adminfoodbanks' id="navlist">View Food Banks</Link>
+                            </ul>
+                        </NavItem>
+                    </ul>
+                </Nav>
+                <NavbarText>
+                    <Button id="btn-logout" className=" btn btn-primary btn-customized"  onClick= {handleLogout} color='danger'  > Logout</Button>
+                </NavbarText>
+            </Navbar>
+            <Switch>
+
+               <Route path = '/login' component={Login}/>
+              
+            </Switch>
 
 </div>
 )
