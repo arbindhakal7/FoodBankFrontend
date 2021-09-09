226a8cd6b9ae6056c0fc0eba413baf032a2b4cf9
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-09 08:44:04 +0545

    constructors and functions

--- src/Components/Admin/AdminAddFoodBank.js
+++ src/Components/Admin/AdminAddFoodBank.js
@@ -2,6 +2,34 @@ import React, {Component} from 'react'
 import AdminNavBar from './AdminNavBar'
 
 export default class AdminAddFoodBank extends Component{
+
+    
+   constructor(props) {
+    super(props)
+    this.state = {
+        FoodBankName: '',
+        availableFood:'',
+        config: {
+            headers: { 'Authorization': localStorage.getItem('token') }
+        }
+    }
+}
+handleChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+        
+    }, ( ) => console.log(this.state))
+}
+handleSubmit = (event) => {
+    event.preventDefault();
+    if(window.confirm('Are you sure to add this blood bank on the list?'))
+    axios.post('http://localhost:90/api/FoodBank', this.state,this.state.config)
+        .then((res) => {
+            this.props.history.push('/admindashboard/adminfoodbanks')
+        }).catch(err => console.log(err.response.data))
+}
+
+
     render(){
         return(  
             <div>
